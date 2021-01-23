80050afd14b334ea4b102116a54d49c65ba41d55
Trung Kien <kien123456k@gmail.com>
2021-01-24 01:22:28 +0700

    litte refactor in user mocking test

--- src/test/repositories/user.spec.ts
+++ src/test/repositories/user.spec.ts
@@ -2,16 +2,21 @@ import {UserRepository} from './../../repositories/user.repository';
 import {User} from '../../entities/User.entity';
 import {setupContainer} from '../../helpers/container.helper';
 
-const container = setupContainer();
-const user = new User();
-
 describe('User Repositories', () => {
+  const container = setupContainer();
+  const user = new User();
+
   beforeAll(() => {
-    user.id = 'a705ea76-9f91-4e4c-913c-66db91f8a014';
-    user.username = 'admin';
-    user.password =
-      '$2a$10$BDv/Ov8A5tVyK.G3M1BaXOI3eq/9xcICsUeHLfwMEvdSZdYTo9t5O';
-    user.fullName = 'F-Code';
+    const mockUser = {
+      id: 'a705ea76-9f91-4e4c-913c-66db91f8a014',
+      username: 'admin',
+      password: '$2a$10$BDv/Ov8A5tVyK.G3M1BaXOI3eq/9xcICsUeHLfwMEvdSZdYTo9t5O',
+      fullName: 'F-Code'
+    };
+
+    Object.keys(mockUser).forEach((key) => {
+      user[key] = mockUser[key];
+    });
 
     const result: Promise<User> = new Promise((resolve, reject) => {
       return resolve(user);
